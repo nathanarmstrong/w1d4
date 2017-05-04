9e784f9deb4487d4e80eb8ed09d2ff8cb639d9e8
nathanarmstrong <nathan.armstrong21@gmail.com>
2017-05-04 22:06:50 +0000

    array with words length

---
+++ mymap.js
@@ -0,0 +1,31 @@
+var words = ["ground", "control", "to", "major", "tom"];
+
+// map(words, function(word) {
+//   return word.length;
+// });
+
+function myMap(words) {
+  wordlength = words.length;
+  return wordlength;
+  }
+  var arr =[];
+    for(i=0; i<words.length; i++){
+    arr.push(myMap(words[i]));
+  }
+
+
+console.log(arr);
+
+
+
+
+
+// function myMap(word) {
+//   for(i=0; i<words.length; i++){
+//     console.log(words[i].length)
+//   }
+
+
+// }
+
+// myMap([words])
